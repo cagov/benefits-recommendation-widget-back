0d52d8b4c6158a7ce2b26d6f6e6812a6d6831d4f
Aaron Hans <aaronhans@gmail.com>
2022-12-05 15:42:45 -0800

    add new logic for shuffling and stuff

--- src/http/get-benefits/index.js
+++ src/http/get-benefits/index.js
@@ -6,10 +6,24 @@ exports.handler = arc.http.async(handler)
 async function handler (req) {
   console.log('calling /benefits get')
   const jsonResponse = fs.readFileSync('./links.json','utf8');
-  //Todo: create translated versions of link content, send translaterd version based on passed in language
+
+  if(jsonResponse.links.length > 3){
+    // mutate link set to be the shuffled top 3
+    let chosenLinks = shuffle(jsonData.links).slice(0,3);
+    jsonData.links = chosenLinks;
+    // identify this set of links by a variation key composed of all the keys of the items in order
+    chosenLinks.forEach((item, index) => { 
+      jsonData.variation += `${item.key}`;
+      if(index < 2) {
+        jsonData.variation += '-';
+      }
+    })
+  }
 
   return {
     cors: true,
     json: jsonResponse
   }
 }
+
+function shuffle(o){ for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x); return o; };
